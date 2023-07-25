11fceab5270a18f85606d9673cbb45d2b9a86cb7
Mary-Maina <richellwanjiru@gmail.com>
2023-07-25 11:52:06 -0700

    print conversion specifiers

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int my_print(const char *design, ...);
 int strlength(const char *s);
 int formstring(va_list sling);
 int charform(va_list champ);
-int print_unsign(unsigned int i);
+int print_unsign(va_list argum);
 int _putchar(char c);
 int numsform(va_list nums);
 
